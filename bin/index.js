2a80707736879de4724ef85d66b6708b0ee30415
Guy Sherman <guy@guysherman.com>
2021-12-28 12:30:11 +1300

    Make sure console.error output is redirected to a log file when run as km

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,7 @@
 #! /usr/bin/env node
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require('fs');
 
+let errlog = fs.createWriteStream('/tmp/kittymux.log');
+process.stderr.write = errlog.write.bind(errlog);
 require('../dist/index.js');
