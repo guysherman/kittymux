faf68e837b76233b1aeaa440f0ff98f691418845
Guy Sherman <guy@guysherman.com>
2022-03-02 09:04:16 +1300

    Add shortcut keys to loadSession

--- src/connectors/sessions/loadSession.ts
+++ src/connectors/sessions/loadSession.ts
@@ -1,11 +1,17 @@
-import { createWindow, sendCommand, stateDir } from '../kitty';
-import { readFileSync } from 'fs';
+import { createWindow, sendCommand, stateDir, WindowListEntryType } from '../kitty';
+import { readFileSync, writeFileSync } from 'fs';
 import { Session } from '../../models/Sessions';
 
 const loadSession = async (sessionName: string): Promise<void> => {
+  const storePath = `${stateDir}/kittymux/quicknavs.json`;
   const sessionPath = `${stateDir}/kittymux/${sessionName}.json`;
   const sessionText = readFileSync(sessionPath, 'utf8');
   const sessionData: Session = JSON.parse(sessionText || '{}');
+
+  const storeText = readFileSync(storePath, 'utf8');
+  const storeData = JSON.parse(storeText || '{}');
+  const { quickNavKeys } = storeData;
+  console.log('quickNavKeys  -pre', { quickNavKeys });
 
   const { windows } = sessionData;
   for (let i = 0; i < windows.length; i++) {
@@ -19,7 +25,19 @@ const loadSession = async (sessionName: string): Promise<void> => {
     if (window?.foregroundProcess) {
       await sendCommand(window.foregroundProcess.args, windowId);
     }
+
+    if (window.shortcutKey) {
+      const { shortcutKey } = window;
+      if (!quickNavKeys[shortcutKey]) {
+        quickNavKeys[shortcutKey] = [];
+      }
+
+      quickNavKeys[shortcutKey].push({ id: windowId, type: WindowListEntryType.Window });
+    }
   }
+
+  console.log('quickNavKeys', { quickNavKeys });
+  writeFileSync(storePath, JSON.stringify({ quickNavKeys }));
 
   return;
 };
