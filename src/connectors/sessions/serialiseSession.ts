c7e190327302830a57e1583c8705cb534bf5cb17
Guy Sherman <guy@guysherman.com>
2022-03-02 10:42:48 +1300

    Exclude foreground process if same as window, and exclude km window

--- src/connectors/sessions/serialiseSession.ts
+++ src/connectors/sessions/serialiseSession.ts
@@ -5,24 +5,27 @@ import { WindowListEntry, WindowListEntryType, stateDir } from '../kitty';
 export const serialiseSession = (entry: WindowListEntry, quickNavKeys: Record<string, QuickNavHandle[]>): void => {
   const { kittyTab } = entry;
 
-  const windows = kittyTab?.windows.map(({ title, cwd, foreground_processes, id }) => {
-    const entry = Object.entries(quickNavKeys).find(([, handles]) =>
-      handles.find((handle) => handle.id === id && handle.type === WindowListEntryType.Window),
-    );
-    const foregroundProcess = foreground_processes.length
-      ? {
-          cwd: foreground_processes[0].cwd,
-          args: foreground_processes[0].cmdline,
-        }
-      : undefined;
+  const windows = kittyTab?.windows
+    .filter((window) => !window.is_focused)
+    .map(({ title, cwd, foreground_processes, id, cmdline }) => {
+      const entry = Object.entries(quickNavKeys).find(([, handles]) =>
+        handles.find((handle) => handle.id === id && handle.type === WindowListEntryType.Window),
+      );
+      const foregroundProcess =
+        foreground_processes.length && foreground_processes[0].cmdline[0] !== cmdline[0]
+          ? {
+              cwd: foreground_processes[0].cwd,
+              args: foreground_processes[0].cmdline,
+            }
+          : undefined;
 
-    return {
-      title,
-      cwd,
-      shortcutKey: entry?.[0] ?? '',
-      foregroundProcess,
-    };
-  });
+      return {
+        title,
+        cwd,
+        shortcutKey: entry?.[0] ?? '',
+        foregroundProcess,
+      };
+    });
 
   const tabEntry = Object.entries(quickNavKeys).find(([, handles]) =>
     handles.find((handle) => handle.id === entry.id && handle.type === WindowListEntryType.Tab),
