3aeafeb771ae6bf3ad419cea93f47d8e33d6f816
Guy Sherman <guy@guysherman.com>
2022-03-02 21:28:50 +1300

    Handle race conditions in tab creation

--- src/connectors/sessions/loadSession.ts
+++ src/connectors/sessions/loadSession.ts
@@ -1,6 +1,32 @@
-import { createWindow, sendCommand, stateDir, WindowListEntryType } from '../kitty';
+import {
+  createWindow,
+  kittyCommand,
+  KittyTab,
+  listWindows,
+  sendCommand,
+  stateDir,
+  WindowListEntryType,
+} from '../kitty';
 import { readFileSync, writeFileSync } from 'fs';
 import { Session } from '../../models/Sessions';
+
+const findTab = async (): Promise<KittyTab | undefined> => {
+  const windowList = await listWindows();
+  const window = windowList.find((window) => window.is_focused);
+  if (window) {
+    const tab = window.tabs.slice(-1)[0];
+    return tab;
+  }
+
+  return undefined;
+};
+
+const delay = (timeout: number): Promise<void> => {
+  const p = new Promise<void>((resolve) => {
+    setTimeout(resolve, 3000);
+  });
+  return p;
+};
 
 const loadSession = async (sessionName: string): Promise<void> => {
   const storePath = `${stateDir}/kittymux/quicknavs.json`;
@@ -14,13 +40,31 @@ const loadSession = async (sessionName: string): Promise<void> => {
   console.log('quickNavKeys  -pre', { quickNavKeys });
 
   const { windows } = sessionData;
+  let tab: KittyTab | undefined = undefined;
   for (let i = 0; i < windows.length; i++) {
     const window = windows[i];
+    // This is a bit gross, but on the first iteration we set newTab, and a title,
+    // on the subsequent iterations we set an id.
     const windowId = await createWindow(window.title, {
       newTab: i === 0,
-      tabTitle: sessionData.title,
+      tabTitle: i === 0 ? sessionData.title : undefined,
+      tabId: i !== 0 ? tab?.id : undefined,
       cwd: window.cwd,
     });
+
+    // On the first window, we need to grab the tab, and use it's id.
+    if (i === 0) {
+      do {
+        console.log('findingTab...');
+        tab = await findTab();
+        if (!tab || tab.title !== sessionData.title || !tab.is_focused) {
+          console.log('Wrong/no tab', { tab });
+          await delay(1000);
+          continue;
+        }
+        await kittyCommand(['focus-tab', '-m', `id:${tab?.id}`]);
+      } while (!tab);
+    }
 
     if (window?.foregroundProcess) {
       await sendCommand(window.foregroundProcess.args, windowId);
