14c69da39e489ae0594df38c283a3670d57c3849
Guy Sherman <guy@guysherman.com>
2022-06-06 21:13:01 +1200

    Added next/prev Tab

--- go/navigatemode.go
+++ go/navigatemode.go
@@ -1,11 +1,18 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/guysherman/kittymux/kitty"
+)
 
 func NavigateModeUpdate(m model, msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch keypress := msg.String(); keypress {
+		case "J":
+			return nextTabPressed(m)
+		case "K":
+			return prevTabPressed(m)
 		case "a":
 			m.mode = Rename
 			m.input.Focus()
@@ -21,6 +28,32 @@ func NavigateModeUpdate(m model, msg tea.Msg) (tea.Model, tea.Cmd) {
 			return m, cmd
 		}
 	}
+	return m, nil
+}
+
+func nextTabPressed(m model) (tea.Model, tea.Cmd) {
+	items := m.list.Items()
+	for i := m.list.Index() + 1; i < len(items); i++ {
+		item := items[i].(item)
+		if item.listEntry.EntryType == kitty.Tab {
+			m.list.Select(i)
+			break
+		}
+	}
+
+	return m, nil
+}
+
+func prevTabPressed(m model) (tea.Model, tea.Cmd) {
+	items := m.list.Items()
+	for i := m.list.Index() - 1; i >= 0; i-- {
+		item := items[i].(item)
+		if item.listEntry.EntryType == kitty.Tab {
+			m.list.Select(i)
+			break
+		}
+	}
+
 	return m, nil
 }
 
