bc18f272ff70f48146391b02845ba49b5a375750
Guy Sherman <guy@guysherman.com>
2022-06-11 18:02:22 +1200

    Move some more list functionality into `listitemmodel.go`

--- go/listitemmodel.go
+++ go/listitemmodel.go
@@ -24,3 +24,41 @@ func findEntry(l list.Model, shortcutKey string) (ListItemModel, bool) {
 
 	return ListItemModel{}, false
 }
+
+func getTabIdForListItem(i ListItemModel) int {
+	tabId := 0
+	switch i.listEntry.EntryType {
+	case kitty.OsWindow:
+		break
+	case kitty.Tab:
+		tabId = i.listEntry.Id
+	case kitty.Window:
+		tabId = i.listEntry.Tab.Id
+	}
+
+	return tabId
+}
+
+func collectItemsForTabId(l list.Model, tabId int) []ListItemModel {
+	items := []ListItemModel{}
+	listItems := l.Items()
+	for _, i := range listItems {
+		listItem := i.(ListItemModel)
+		switch listItem.listEntry.EntryType {
+		case kitty.OsWindow:
+			break
+		case kitty.Tab:
+			if listItem.listEntry.Id == tabId {
+				items = append(items, listItem)
+			}
+			break
+		case kitty.Window:
+			if listItem.listEntry.Tab.Id == tabId {
+				items = append(items, listItem)
+			}
+			break
+		}
+	}
+
+	return items
+}
--- go/updatequicknavcmd.go
+++ go/updatequicknavcmd.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/guysherman/kittymux/kitty"
 	"github.com/guysherman/kittymux/settings"
 )
 
@@ -15,7 +14,7 @@ func updateQuickNav(m UiModel, i ListItemModel, keypress string) tea.Cmd {
 
 		qndb := m.qndb
 		tabId := getTabIdForListItem(i)
-		sameTabItems := collectItemsForTabId(m, tabId)
+		sameTabItems := collectItemsForTabId(m.list, tabId)
 
 		for _, i := range sameTabItems {
 			if i.shortcutKey == keypress {
@@ -32,42 +31,4 @@ func updateQuickNav(m UiModel, i ListItemModel, keypress string) tea.Cmd {
 			qndb: qndb,
 		}
 	}
-}
-
-func getTabIdForListItem(i ListItemModel) int {
-	tabId := 0
-	switch i.listEntry.EntryType {
-	case kitty.OsWindow:
-		break
-	case kitty.Tab:
-		tabId = i.listEntry.Id
-	case kitty.Window:
-		tabId = i.listEntry.Tab.Id
-	}
-
-	return tabId
-}
-
-func collectItemsForTabId(m UiModel, tabId int) []ListItemModel {
-	items := []ListItemModel{}
-	listItems := m.list.Items()
-	for _, i := range listItems {
-		listItem := i.(ListItemModel)
-		switch listItem.listEntry.EntryType {
-		case kitty.OsWindow:
-			break
-		case kitty.Tab:
-			if listItem.listEntry.Id == tabId {
-				items = append(items, listItem)
-			}
-			break
-		case kitty.Window:
-			if listItem.listEntry.Tab.Id == tabId {
-				items = append(items, listItem)
-			}
-			break
-		}
-	}
-
-	return items
 }
